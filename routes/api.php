2a97b5f6cc69f54d38c78e4aa7f97a1013011aea
zidane.nur004 <zidane.nur004@gmail.com>
2025-04-09 14:21:51 +0000

    test project 2

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,9 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',[AuthController::class, 'login']);
+Route::get('test', function() {
+    return response(["messgae" => "test"]);
+});
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login'])->name('login');
 
