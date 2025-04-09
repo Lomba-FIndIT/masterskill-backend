c373cd6cf88962e3924c30ce7c04f99abed09232
zidane.nur004 <zidane.nur004@gmail.com>
2025-04-09 14:35:31 +0000

    api ganti ke web

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/test', function () {
+    return response(["message" => "test"]);
+});
+Route::middleware('auth:sanctum')->group(function() {
+    Route::get('logout', [AuthController::class, 'logout']);
 });
